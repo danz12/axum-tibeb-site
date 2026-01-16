67f8929459a59539d99ab293b2b0355190f194b5
danz12 <danielnegus2009@gmail.comgit config --global user.email danielnegus2009@gmail.comgit config --global user.email danielnegus2009@gmail.com>
2026-01-16 21:17:34 +0300

    Add GitHub Pages deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   root: ".",
+  base: "/axum-tibeb-site/",
   server: {
     host: true,
     port: 8085,
